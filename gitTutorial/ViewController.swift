26a71db2139810a58a9306d312badea15b84c977
Simon Gomez <simongomez636@gmail.com>
2020-10-08 09:24:00 -0500

    Initial Commit

---
+++ gitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTutorial
+//
+//  Created by Gomez, Simon on 10/8/20.
+//  Copyright © 2020 Gomez, Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
